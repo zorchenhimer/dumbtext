96b4c7b4bba1b2b69070b298d2b74c40dc62111f
Zorchenhimer <zorchenhimer@gmail.com>
2023-04-12 19:41:33 -0400

    A bunch of polish
    - Take input from the command line, an input file, or STDIN.
    - Add some help text

--- main.go
+++ main.go
@@ -3,53 +3,114 @@ package main
 import (
 	"fmt"
 	"os"
+	"io"
 	"math/rand"
 	"strings"
 	"time"
+	"bytes"
+
+	"github.com/alexflint/go-arg"
 )
 
+type Options struct {
+	Input []string `arg:"positional" placeholder:"text" help:"Text to transform"`
+	InputFile string `arg:"-i,--input" placeholder:"input.txt" help:"File containing text to transform"`
+}
+
+func (o *Options) Description() string {
+	return "Shuffle the case of text"
+}
+
 func main() {
-	if len(os.Args) == 1 {
-		return
+	opts := &Options{}
+	arg.MustParse(opts)
+
+	err := run(opts)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+
+func run(opts *Options) error {
+	if opts.Input != nil {
+		fmt.Println(convert(true, strings.Join(opts.Input, " ")))
+		return nil
 	}
 
+	if opts.InputFile != "" {
+		//raw, err := os.ReadFile(opts.InputFile)
+		file, err := os.Open(opts.InputFile)
+		if err != nil {
+			return fmt.Errorf("Unable to read file: %w", err)
+		}
+		return streamConvert(file, os.Stdout)
+	}
+
+	return streamConvert(os.Stdin, os.Stdout)
+}
+
+func streamConvert(input io.Reader, output io.Writer) error {
+	buf := &bytes.Buffer{}
+	buf.Grow(2048)
+	var err error
+	var n int64
+	first := true
+
+	for err != io.EOF {
+		n, err = io.Copy(buf, input)
+		if err != nil && err != io.EOF {
+			return fmt.Errorf("Error reading input: %w", err)
+		}
+		if n == 0 {
+			return nil
+		}
+
+		fmt.Fprint(output, convert(first, string(buf.Bytes()[:buf.Len()])))
+		buf.Reset()
+		first = false
+	}
+
+	return nil
+}
+
+func convert(first bool, input string) string {
 	// keep track of last two cases.  We don't want long sequences of one case.
 	last1, last2 := false, false
 
 	rng := rand.New(rand.NewSource(time.Now().Unix()))
-	words := []string{}
-	for i, a := range os.Args [1:] {
-		a = strings.ToLower(a)
-		word := []string{}
+	chars := []string{}
 
-		for _, c := range a {
-			cs := string(c)
-			this := false
+	for _, c := range input {
+		cs := string(c)
+		this := false
 
-			// Don't bother with non-alpha characters
-			if !((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
-				word = append(word, cs)
-				continue
-			}
+		// Don't bother with non-alpha characters
+		if !((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+			//word = append(word, cs)
+			chars = append(chars, cs)
+			continue
+		}
 
-			// if last two were lower, upper next (and vice versa), execept if
-			// first char.
-			if i > 0 && last1 == last2 {
-				// in here so we get both upper and lowercase without dropping
-				// to the else if below
-				if !last1 {
-					cs = strings.ToUpper(cs)
-					this = true
-				}
-			} else if rng.Int() % 2 == 0 {
+		// if last two were lower, upper next (and vice versa), execept if
+		// first char.
+		if !first && last1 == last2 {
+			// in here so we get both upper and lowercase without dropping
+			// to the else if below
+			if !last1 {
 				cs = strings.ToUpper(cs)
 				this = true
 			}
-
-			last2, last1 = last1, this
-			word = append(word, cs)
+		} else if rng.Int() % 2 == 0 {
+			cs = strings.ToUpper(cs)
+			this = true
 		}
-		words = append(words, strings.Join(word, ""))
+
+		last2, last1 = last1, this
+		//word = append(word, cs)
+		chars = append(chars, cs)
+		first = false
 	}
-	fmt.Println(strings.Join(words, " "))
+	return strings.Join(chars, "")
 }
